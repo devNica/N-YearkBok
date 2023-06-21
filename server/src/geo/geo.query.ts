4039e7c21f508f20c81e295f35aba4bf99d334e4
devNica <designsdevnicaragua@gmail.com>
2023-06-21 08:57:04 -0600

    :ambulance: corregir error en consulta de provincias por estado de una region

--- server/src/geo/geo.query.ts
+++ server/src/geo/geo.query.ts
@@ -29,22 +29,23 @@ export const fetchStateByRegionQuery = (opt: string): string => `
 `
 
 export const fetchProvincesByStateFromRegionQuery = (opt: string): string => `
-    SELECT 
-
+    SELECT
     mr.id,
     mr.extension,
     mr.name,
     mu.unit,
-    json_agg(jsonb_build_object('state', s.name, 'stateId', s.id, 'provinces', 
-    (SELECT json_agg(json_build_object('provinceId', p.id, 'province', p.name)) 
-    FROM province p WHERE p.state_id = s.id))) as "states"
-
-    FROM macro_region mr 
-    INNER JOIN measurement_unit mu ON mu.id  = mr.measurement_id
+    json_agg(jsonb_build_object('state', s.name, 'stateId', s.id, 'provinces', subquery.provinces)) AS "states"
+    
+    FROM macro_region mr
+    INNER JOIN measurement_unit mu ON mu.id = mr.measurement_id
     INNER JOIN state s ON s.region_id = mr.id
-    INNER JOIN province p ON p.state_id = s.id
-    ${opt} 
-    GROUP BY mr.id, mr.name, mu.unit
+    LEFT JOIN (
+    SELECT DISTINCT p.state_id, jsonb_agg(jsonb_build_object('provinceId', p.id, 'province', p.name) ORDER BY p.id) AS provinces
+    FROM province p
+    GROUP BY p.state_id
+    ) subquery ON subquery.state_id = s.id
+    ${opt}
+    GROUP BY mr.id, mr.extension, mr.name, mu.unit
 `
 
 export const fetchProvincesByRegionQuery = (opt: string): string => `
